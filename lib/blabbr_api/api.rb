b0c57f14974bb0e7266abd295131f67323c58782
chatgris <jboyer@af83.com>
2012-03-03 14:25:57 +0100

    Allow to update current_user.
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/blabbr_api/api.rb
+++ lib/blabbr_api/api.rb
@@ -13,6 +13,16 @@ class BlabbrApi::Api < Grape::API
     current_user
   end
 
+  put '/me' do
+    # TODO: expose errors in a cleaner way
+    user = BlabbrCore::User.new(current_user, current_user.limace)
+    if user.update(params[:user])
+      current_user.reload
+    else
+      error! user.send(:resource).errors.as_json, 412
+    end
+  end
+
   resource :users do
     get do
       BlabbrCore::UsersCollection.new(current_user).all.to_a
--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -21,17 +21,39 @@ describe BlabbrApi::Api, type: :requests do
       last_response.status.should == 200
       JSON.parse(last_response.body).first['nickname'].should == current_user.nickname
     end
+  end
 
+  describe 'GET /v1/users/:user' do
     it 'finds a user' do
       get "/v1/users/#{current_user.nickname}", {}, {'current_user' => current_user}
       last_response.status.should eq 200
       JSON.parse(last_response.body)['nickname'].should eq current_user.nickname
     end
+  end
 
+  describe 'GET /me' do
     it 'finds current_user' do
       get "/v1/me", {}, {'current_user' => current_user}
       last_response.status.should eq 200
       JSON.parse(last_response.body)['nickname'].should eq current_user.nickname
+    end
+  end
+
+  describe 'PUT /me' do
+    context 'with valid params' do
+      it 'updates current_user' do
+        put "/v1/me", {user: {nickname: 'New test'}}, {'current_user' => current_user}
+        last_response.status.should eq 200
+        JSON.parse(last_response.body)['nickname'].should eq 'New test'
+      end
+    end
+
+    context 'with invalid params' do
+      it 'updates current_user' do
+        put "/v1/me", {user: {nickname: 'a'}}, {'current_user' => current_user}
+        last_response.status.should eq 412
+        JSON.parse(last_response.body).keys.should include 'nickname'
+      end
     end
   end
 
